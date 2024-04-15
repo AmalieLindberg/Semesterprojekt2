ba2c2a47b37c6accc495f6b0938389ae83b4cb10
AmalieLindberg <aml001@edu.zealand.dk>
2024-04-15 10:04:26 +0200

    Shop HTML

---
+++ Semesterprojekt2/Pages/Shop/Brushes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Semesterprojekt2.Pages.Shop
+{
+    public class BrushesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
