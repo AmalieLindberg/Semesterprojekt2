94242a87e94a5a060082bddcc412d13ee140c2b1
Fan9e <Fannie.holm@hotmail.com>
2024-04-30 13:51:51 +0200

    Kommentar til Login C#

--- Semesterprojekt2/Pages/Login/Login.cshtml.cs
+++ Semesterprojekt2/Pages/Login/Login.cshtml.cs
@@ -11,48 +11,54 @@ namespace Semesterprojekt2.Pages.Login
 {
     public class LoginModel : PageModel
     {
-
+        // Gemmer den aktuelle logget ind bruger
         public static User LoggedInUser { get; set; } = null;
+        // Reference til IUserService interface
         private IUserService _userService;
-
+        //BindProperty til email-inputfeltet på login-siden
         [BindProperty]
         public string Email { get; set; }
-
+        //Bindproperty til pasword-inputfeltet på login-siden markeret som passwrd type
         [BindProperty, DataType(DataType.Password)]
         public string Password { get; set; }
+        //Besked der vises ved uyldig logins
         public string Message { get; set; }
-
+        //Konstruktør, der modtager en instance af IUserService
         public LoginModel(IUserService userService)
         {
             _userService = userService;
         }
-
+        //Metode kaldt ved HTTP GET-anmodning til login-siden, 
         public void OnGet()
         {
+            //intet gøres 
         }
-
+        //Metode kaldt ved HTTP POST-anmodning fra logind-formularen
         public async Task<IActionResult> OnPost()
         {
-
+            //Hent en liste af brugere fra IUserService
             List<User> users = _userService.GetUsers();
+            //Gennemgå hver bruger i listen
             foreach (User user in users)
             {
-
+                //tjek om indtastet email og password matcher en bruger i lsten
                 if (Email == user.Email && Password == user.Password)
                 {
-
+                    //Gem den logget ind bruger
                     LoggedInUser = user;
-
+                    //Opret en claim for brugerens email
                     var claims = new List<Claim> { new Claim(ClaimTypes.Email, Password) };
-
+                    //Opret en ClaimIdentity med email-claiet
                     var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    //Log brugeren in ved at oprette et cookie og tiltøje en climIdentety
                     await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+                    //Redirect til startsiden
                     return RedirectToPage("/Index");
 
                 }
 
             }
-
+            //Hvis ingen brugere blev fundet med de oplysninger, så for man en fejlbesked istedet
             Message = "Invalid attempt";
             return Page();
         }
