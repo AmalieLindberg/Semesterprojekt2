73322a4082fae0fc8efcd6d1230736a2a99baeb9
AmalieLindberg <aml001@edu.zealand.dk>
2024-05-07 11:31:47 +0200

    Update MockProducts.cs

--- Semesterprojekt2/MockData/Shop/MockProducts.cs
+++ Semesterprojekt2/MockData/Shop/MockProducts.cs
@@ -5,7 +5,7 @@ namespace Semesterprojekt2.MockData.Shop
     {
         private static List<Product> products = new List<Product>() 
         {
-            new Product("Lavender Shampoo", 59.5, ProductType.Soap, "Nivea", 1,"Shampoo with smell of lavender"),
+            new Product("Lavender Shampoo", 59, ProductType.Soap, "Nivea", 1,"Shampoo with smell of lavender"),
             new Product("Lever Treats", 150, ProductType.Treats, "Dog Treats", 200,"Treats with a taste of lever"),
             new Product("Squeaky Cat", 120, ProductType.Toy, "ToysRUs", 1,"A toy cat, that makes a squeaky sound when the dog bites it.")
         };
