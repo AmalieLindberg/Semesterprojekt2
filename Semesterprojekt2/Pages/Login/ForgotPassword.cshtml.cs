7ebb55750fee289fcc4169ee9921f37c335b3908
Clara Neu <mariaclara.neu@gmail.com>
2024-05-21 10:23:14 +0200

    Styling Changes + ForgotPassword Page

---
+++ Semesterprojekt2/Pages/Login/ForgotPassword.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Semesterprojekt2.Pages.Login
+{
+    public class ForgotPasswordModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
